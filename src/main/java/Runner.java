b64b33a4a9e93b74f5cd1c1548eddc429f33145b
GooiestGoober <octavio.cervera@allsaintshigh.org>
2025-10-30 10:45:10 -0500

    Stringer labster

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,20 @@
 public class Runner {
     public static void main(String[] args){
-
+        String i = "Dog";
+        String b = "aplus";
+        String c = "cato";
+        int inty = 3;
+        double avg1 = StringySpells.aveLen(i, b);
+        System.out.println("aveLen of " + i + " and " + b + ": " + avg1);
+        String avg2 = StringySpells.middleFinder(i);
+        System.out.println("middleFinder of " +i + ": " + avg2);
+        String avg67 = StringySpells.middleFinder(c);
+        System.out.println("middleFinder of " +c + ": " + avg67);
+        String  avg3 = StringySpells.alphaOmega(i,b);
+        System.out.println("alphaOmega of " + i + " and " + b+ ": " + avg3);
+        String avg4 = StringySpells.flipper(i,b);
+        System.out.println("flipper of " + i + " and " + b+ ": " + avg4);
+        String avg5 = StringySpells.subtractive(b,inty);
+        System.out.println("subtractive of " + b + " and " + inty + ": " + avg5);
     }
 }
--- src/main/java/StringySpells.java
+++ src/main/java/StringySpells.java
@@ -2,15 +2,37 @@
 
 public class StringySpells {
 
-    // middleFinder
+    public static String middleFinder(String i){
+        int len1 = i.length();
+        int mid1 = len1 / 2;
+        return i.substring(mid1,mid1+1);
 
-    // alphaOmega
+    }
 
-    // subtractive
+    public static String alphaOmega(String a, String b){
+        String alpha = a.substring(0,1);
+        int len1 = b.length();
+        String Omega = b.substring(len1-1,len1);
+        return alpha+Omega;
+    }
 
-    // flipper
+    public static String subtractive(String a, int b){
 
-    // aveLen
+        return a.substring(0,b) + a.substring(b+1);
+    }
+
+    public static String flipper(String i, String b){
+        String dos = b.substring(0,1);
+        String unos = i.substring(1);
+        return dos+unos;
+    }
+
+    public static double aveLen(String i, String d){
+        double len1 = i.length();
+        double len2 = d.length();
+        return (len1 + len2)/2;
+
+    }
 
 
-}//end of class
+}
